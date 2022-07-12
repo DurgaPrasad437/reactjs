6e6400ac106996e89e348d542c8270baefed2769
Durga Prasad <d.durga.prasad@accenture.com>
2022-07-12 18:39:12 +0530

    code uploaded today

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -24,11 +24,37 @@ import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput'
+import FRParentInput from './components/FRParentInput';
+import PortalDemo from './components/PortalDemo';
+import Hero from './components/Hero';
+import ErrorBoundary from './components/ErrorBoundary';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
+
 class App extends Component {
   render() {
    return (
     <div className="App">
-      <FocusInput />
+      
+    <ClickCounter name ='Viswas' />
+    <HoverCounter />
+
+    {/* 
+    <ErrorBoundary >
+    <Hero heroName ="Batman" />
+    </ErrorBoundary >
+
+    <ErrorBoundary>
+    <Hero heroName ="Superman" />
+    </ErrorBoundary>
+
+       <ErrorBoundary>
+    <Hero heroName ="Joker" />
+    </ErrorBoundary>*/}
+
+    {/* <PortalDemo />
+      <FRParentInput />
+     {/* <FocusInput />
      {/*<RefsDemo />
       {/* <ParentComp />
       {/*<Table />
