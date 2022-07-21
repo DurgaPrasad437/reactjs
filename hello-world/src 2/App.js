03063eae5153e878a6ffe1d4b383836694c12805
Durga Prasad <d.durga.prasad@accenture.com>
2022-07-21 19:43:27 +0530

    Code

---
+++ hello-world/src 2/App.js
@@ -0,0 +1,53 @@
+import React, { useReducer } from 'react'
+import './App.css';
+import ComponentA from './components/ComponentA';
+import ComponentB from './components/ComponentB';
+import ComponentC from './components/ComponentC';
+
+export const CountContext = React.createContext()
+
+{/* import CounterThree from './components/CounterThree';
+import ComponentC from './components/ComponentC'; 
+import CounterOne from './components/CounterOne';
+import CounterTwo from './components/ConterTwo';*/}
+
+const initialState = 0
+const reducer = (state, action) => {
+ switch(action) {
+   case 'increment':
+   return state + 1
+   case 'decrement':
+   return state -1
+   case 'reset' :
+  return initialState
+  default:
+      return state 
+ }
+}
+
+function App() {
+  const[count, dispatch] = useReducer(reducer, initialState)
+  return (
+    <CountContext.Provider
+    value ={{countState: count, countDispatch: dispatch }}
+    >
+    <div className="App">
+      Count -{count}
+      <ComponentA />
+      <ComponentB />
+      <ComponentC />
+
+      {/*  <CounterThree />
+     {/* <CounterTwo />
+     {/*  <CounterOne />
+    {/* <Usercontext.Provider value={'vishwas'}>
+        <Channelcontext.Provider value={'codeevolution'}>
+        <ComponentC/>
+        </Channelcontext.Provider>
+  </Usercontext.Provider> */}
+    </div>
+    </CountContext.Provider>
+  );
+}
+
+export default App;
---
+++ hello-world/src 3/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import DataFetchingOne from './components/DataFechingOne';
+import DataFetchingTwo from './components/DataFetchingTwo';
+import ParentComponent from './components/ParentComponent';
+import Counter from './components/Counter';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Counter />
+     {/*  <ParentComponent />
+     {/* <DataFetchingTwo />
+    {/*<DataFetchingOne /> */}
+    </div>
+  );
+}
+
+export default App;
